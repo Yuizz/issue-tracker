67192a3658185d1a0d77e6fd8847bf6cd60b18bb
Jugo <julianglz@outlook.es>
2023-08-05 14:20:08 -0500

    Agregar proyecto formulario terminado

--- src/pages/projects/add.tsx
+++ src/pages/projects/add.tsx
@@ -1,34 +1,44 @@
-import { Button, Input } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Input } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { api } from "~/utils/api";
 
 function add() {
-  const [name, setName] = useState("");
+  const [isFetching, setIsFetching] = useState(false);
+  const { push } = useRouter();
   const mutationProjects = api.projects.create.useMutation();
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(e.currentTarget);
+    setIsFetching(true);
     const formData = new FormData(e.currentTarget);
     const data = {
-      name: formData.get("name"),
+      name: formData.get("name")?.toString() || "",
     };
 
-    console.log(data);
-
-    // const newProject = await mutationProjects.mutate({ name });
-    // console.log(newProject);
+    const newProject = await mutationProjects.mutate(data);
+    setIsFetching(false);
+    push("/me");
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <Input
-        name="name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <Button type="submit">Submit</Button>
-    </form>
+    <Box py="1rem" px="3rem">
+      <form onSubmit={onSubmit}>
+        <Flex flexDir="column" gap="1rem">
+          <Heading size="md">Agregar proyecto</Heading>
+          <Input name="name" required />
+          <Button
+            type="submit"
+            colorScheme="green"
+            w="max-content"
+            isLoading={isFetching}
+            loadingText="Creando proyecto"
+          >
+            Submit
+          </Button>
+        </Flex>
+      </form>
+    </Box>
   );
 }
 
